eb0ca320a49480d2230ceea07c9e83d233fcc97b
coturiv <coturiv@gmail.com>
2025-11-27 06:39:21 +0900

    chore: package rename to @arbitral/article-parser and metadata update

--- src/utils/isValidUrl.js
+++ src/utils/isValidUrl.js
@@ -4,7 +4,7 @@ export default (url = '') => {
   try {
     const ourl = new URL(url)
     return ourl !== null && ourl.protocol.startsWith('http')
-  } catch (err) {
+  } catch {
     return false
   }
 }
